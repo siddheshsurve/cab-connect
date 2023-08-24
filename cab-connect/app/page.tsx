65fbfe63eb8950fe302d93547360bc1330d5f59b
siddheshsurve <sysurve24@gmail.com>
2023-08-24 18:25:46 +0530

    layout -booking & map

--- cab-connect/app/page.tsx
+++ cab-connect/app/page.tsx
@@ -3,8 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
    <div>
-
-    <h2>Hi There</h2>
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-blue-100'>
+        Booking
+      </div>
+      <div className='col-span-2 bg-red-100'>
+        Map
+      </div>
+    </div>
    </div>
   )
 }
