cc990a6b47c857aa77e16e00bfe766f1b981cb70
siddheshsurve <sysurve24@gmail.com>
2023-08-24 16:54:57 +0530

    clerk authentication

--- cab-connect/app/page.tsx
+++ cab-connect/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
    <div>
+    <UserButton afterSignOutUrl="/"/>
     <h2>Hi There</h2>
    </div>
   )
